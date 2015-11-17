f3ee1393d45e6a9fecf37144a62b621a2ca55f72
Venelin Stoykov <venelin@magicsolutions.bg>
2015-11-17 12:26:38 +0200

    Make it possible to disable scanner but show warnings

--- src/django_clamd/conf.py
+++ src/django_clamd/conf.py
@@ -1,4 +1,7 @@
+import warnings
+
 from django.conf import settings as _settings
+
 
 # Default local socket for Debian based systems
 # For Fedora you need to set CLAMD_SOCKET = '/var/run/clamd.scan/clamd.sock'
@@ -11,3 +14,9 @@ CLAMD_USE_TCP = getattr(_settings, 'CLAMD_USE_TCP', False)
 CLAMD_TCP_SOCKET = getattr(_settings, 'CLAMD_TCP_PORT', 3310)
 # Default CLamd TCP socket addres
 CLAMD_TCP_ADDR = getattr(_settings, 'CLAMD_TCP_SOCKET', '127.0.0.1')
+
+# Enable ClamD scanner. By default True. Set to False only for development.
+CLAMD_ENABLED = getattr(_settings, 'CLAMD_ENABLED', True)
+
+if not _settings.DEBUG and not CLAMD_ENABLED:
+    warnings.warn('You are running with DEBUG=False and CLAMD_ENABLED=False. This is probably due to missconfigurations.')
--- src/django_clamd/validators.py
+++ src/django_clamd/validators.py
@@ -1,10 +1,17 @@
+import warnings
+
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from django_clamd import get_scanner
+from .conf import CLAMD_ENABLED
 
 
 def validate_file_infection(file):
+    # If django-clamd is disabled (for debugging) then do not check the file.
+    if not CLAMD_ENABLED:
+        warnings.warn('Runing clamd validator with CLAMD_ENABLED=False')
+        return
     # Ensure file pointer is at begingin of the file
     file.seek(0)
     scanner = get_scanner()
