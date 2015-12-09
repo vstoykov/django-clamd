89ebf01d390a593f1e23baa59b11f130ceabf7fb
Venelin Stoykov <vkstoykov@gmail.com>
2015-12-09 02:17:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme:
 
 setup(
     name="django-clamd",
-    version='0.2.1',
+    version='0.3.0',
     author="Venelin Stoykov",
     author_email="vkstoykov@gmail.com",
     maintainer="Venelin Stoykov",
