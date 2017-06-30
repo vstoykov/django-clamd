db37ba72f771da62819ca8eb09674793d5b27ba4
Venelin Stoykov <venelin@magicsolutions.bg>
2017-06-30 14:57:38 +0300

    Do not raize error when file is too big

--- src/django_clamd/validators.py
+++ src/django_clamd/validators.py
@@ -1,10 +1,14 @@
 import warnings
+import logging
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 from django_clamd import get_scanner
 from .conf import CLAMD_ENABLED
+
+
+logger = logging.getLogger(__name__)
 
 
 def validate_file_infection(file):
@@ -15,7 +19,16 @@ def validate_file_infection(file):
     # Ensure file pointer is at begingin of the file
     file.seek(0)
     scanner = get_scanner()
-    result = scanner.instream(file)
+    try:
+        result = scanner.instream(file)
+    except IOError:
+        # Ping the server if it fails than the server is down
+        scanner.ping()
+        # Server is up. This means that the file is too big.
+        logger.warn('The file is too large for ClamD to scan it. Bytes Read {}'.format(file.tell()))
+        file.seek(0)
+        return
+
     if result and result['stream'][0] == 'FOUND':
         raise ValidationError(_('File is infected with malware.'), code='infected')
     # Return file pointer at initial state
